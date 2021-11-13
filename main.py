3a1d754f9de4afd90dd4a3654a7aa1100d71dc21
Yusuf Arslanalp <ayusufa42@gmail.com>
2021-11-13 10:44:04 +0300

    fead: write data to google sheets with python

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from openpyxl import load_workbook
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
 
 wb = load_workbook(filename = 'URL\'s.xlsx')
 ws = wb.active
@@ -8,10 +14,20 @@ allUrls = []
 i = 1
 currentUrl = ws['A1'].value
 
-
+"""
 while( currentUrl != None ):
     print( currentUrl )
     allUrls.append( currentUrl )
     i += 1
     currentUrl = ws[ 'A' + str(i) ].value
+"""
 
+
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+client = gspread.authorize(creds)
+sheet = client.open("AnalyticaHouse").sheet1  # Open the spreadhseet
+
+
+insertRow = ["hello", 5, "red", "from main file 2222"]
+sheet.append_row( insertRow )
